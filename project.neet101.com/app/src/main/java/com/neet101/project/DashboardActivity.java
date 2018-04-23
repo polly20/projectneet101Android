e0385b99cbc5b547592b9b0c6a6cf5aa8b47c864
DESKTOP-9QJEEPU\paopao <ptxt4wrd@gmail.com>
2018-04-23 22:09:25 +0800

    Added Facebook Login/Registration

---
+++ project.neet101.com/app/src/main/java/com/neet101/project/DashboardActivity.java
@@ -0,0 +1,97 @@
+package com.neet101.project;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+
+import java.io.InputStream;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    public CallbackManager callbackManager;
+
+    public ProfileTracker profileTracker;
+
+    public String userId, firstName, lastName, email, imageUrl;
+
+    public Button btnLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        setTitle("Profile");
+
+        Bundle inBundle = getIntent().getExtras();
+        firstName = inBundle.getString("name");
+        lastName = inBundle.getString("surname");
+        email = inBundle.getString("email");
+        imageUrl = inBundle.getString("imageUrl");
+
+        TextView Fullname = (TextView)findViewById(R.id.Fullname);
+        Fullname.setText(firstName + " " + lastName);
+
+        TextView Email = (TextView)findViewById(R.id.Email);
+        Email.setText(email);
+
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+        new DownloadImageTask((ImageView) findViewById(R.id.profileImage))
+                .execute(imageUrl);
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+    private void logout(){
+        LoginManager.getInstance().logOut();
+        Intent login = new Intent(DashboardActivity.this, MainActivity.class);
+        startActivity(login);
+        finish();
+    }
+
+}
--- project.neet101.com/app/src/main/java/com/neet101/project/MainActivity.java
+++ project.neet101.com/app/src/main/java/com/neet101/project/MainActivity.java
@@ -1,9 +1,11 @@
 package com.neet101.project;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,17 +16,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String EMAIL = "email";
 
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressDialog pDialog;
+
+    public static String fb_url = "";
+
+    public String userId, firstName, lastName, email, birthday, gender;
+
+    public URL profilePicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = (TextView) findViewById(R.id.btnLogin);
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
         btnFBLogin = (Button) findViewById(R.id.btnFBLogin);
-
         btnFBLogin1 = (LoginButton) findViewById(R.id.login_button);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -61,6 +86,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(getBaseContext(), LoginActivity.class);
                 startActivity(i);
+            }
+        });
+
+        btnFBLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnFBLogin1.performClick();
             }
         });
 
@@ -72,10 +104,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         callbackManager = CallbackManager.Factory.create();
-
-        btnFBLogin1 = (LoginButton) findViewById(R.id.login_button);
 
         btnFBLogin1.setReadPermissions(Arrays.asList(EMAIL));
 
@@ -87,6 +116,48 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Status", "Success");
                 Log.d("Facebook_AppID", loginResult.getAccessToken().getApplicationId());
                 Log.d("Facebook_Token", loginResult.getAccessToken().getToken());
+                Log.d("Facebook_UserId", loginResult.getAccessToken().getUserId());
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.e(TAG,object.toString());
+                        Log.e(TAG,response.toString());
+
+                        try {
+                            userId = object.getString("id");
+                            profilePicture = new URL("https://graph.facebook.com/" + userId + "/picture?width=500&height=500");
+                            if(object.has("first_name"))
+                                firstName = object.getString("first_name");
+                            if(object.has("last_name"))
+                                lastName = object.getString("last_name");
+                            if (object.has("email"))
+                                email = object.getString("email");
+                            if (object.has("birthday"))
+                                birthday = object.getString("birthday");
+                            if (object.has("gender"))
+                                gender = object.getString("gender");
+
+                            Intent main = new Intent(MainActivity.this, DashboardActivity.class);
+                            main.putExtra("name",firstName);
+                            main.putExtra("surname",lastName);
+                            main.putExtra("email",email);
+                            main.putExtra("imageUrl",profilePicture.toString());
+                            startActivity(main);
+                            finish();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                //Here we put the requested fields to be returned from the JSONObject
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, first_name, last_name, email, birthday, gender");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
 
             @Override
@@ -101,7 +172,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Status", "Error" + exception.getMessage());
             }
         });
-
     }
 
     @Override
@@ -109,5 +179,80 @@ public class MainActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    private class getFacebookInfo extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(fb_url, "GET");
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    String name = jsonObj.get("name").toString();
+
+                    Log.d("name", name);
+
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+        }
+
+    }
+
 
 }
