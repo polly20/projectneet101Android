838201798bcdbbd2b81e29403004d3fef3e44136
DESKTOP-9QJEEPU\paopao <ptxt4wrd@gmail.com>
2018-05-10 22:10:57 +0800

    26.1

--- project.neet101.com/app/src/main/java/com/neet101/project/TargetActivity.java
+++ project.neet101.com/app/src/main/java/com/neet101/project/TargetActivity.java
@@ -9,5 +9,7 @@ public class TargetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target);
+
+        getSupportActionBar().hide();
     }
 }
