fbc2a9aa8baae869602dc7ac245e348959a74454
DESKTOP-9QJEEPU\paopao <ptxt4wrd@gmail.com>
2018-05-16 23:45:05 +0800

    28

--- project.neet101.com/app/src/main/java/com/neet101/project/RecommendActivity.java
+++ project.neet101.com/app/src/main/java/com/neet101/project/RecommendActivity.java
@@ -2,8 +2,21 @@ package com.neet101.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class RecommendActivity extends AppCompatActivity {
+
+    String TAG = "TAG";
+
+    RadioButton rbt_20, rbt_30, rbt_40, rbt_50, rbt_60, rbt_70, rbt_80, rbt_90, rbt_100;
+
+    Button btnSubmit;
+
+    EditText txtValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +24,30 @@ public class RecommendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recommend);
 
         getSupportActionBar().hide();
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.rg);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                String value = ((RadioButton)findViewById(checkedId)).getText().toString();
+
+                override_target(value);
+            }
+        });
+    }
+
+    public void override_target(String value) {
+        txtValue = (EditText) findViewById(R.id.txtValue);
+        txtValue.setText(value.toString());
+    }
+
+    public String getRadionButton(Integer val) {
+
+        if(val > 0 && val <= 20) {
+
+        }
+
+        return null;
     }
 }
--- project.neet101.com/app/src/main/java/com/neet101/project/TargetActivity.java
+++ project.neet101.com/app/src/main/java/com/neet101/project/TargetActivity.java
@@ -1,15 +1,29 @@
 package com.neet101.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class TargetActivity extends AppCompatActivity {
+
+    String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target);
-
         getSupportActionBar().hide();
+
+
     }
+
+
 }
